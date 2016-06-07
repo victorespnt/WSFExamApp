8fae49ca145d42753daf6993fef2bcfa871aac29
Iman Zarrabian <iman@omts.fr>
2016-06-07 14:30:23 +0200

    Initial commit for exam

---
+++ ExamApp/ExamApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ExamApp
+//
+//  Created by Iman Zarrabian on 07/06/16.
+//  Copyright © 2016 Web School Factory. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
